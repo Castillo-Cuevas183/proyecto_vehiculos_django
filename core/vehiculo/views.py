563ff093edb537be387582f87ff64bcea4c8ce4b
Castillo-Cuevas183 <castillo.cri@gmail.com>
2024-10-19 17:28:27 -0300

    first commit

---
+++ core/vehiculo/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from .forms import VehiculoForm
+from .models import Vehiculo
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import permission_required
+
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+# Decoradores de permisos
+@permission_required('vehiculo.agregar_vehiculo')  # Asegúrate de que el permiso es correcto
+def agregar_vehiculo(request):
+    if request.method == 'POST':
+        form = VehiculoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_vehiculos')
+    else:
+        form = VehiculoForm()
+    return render(request, 'vehiculo/agregar_vehiculo.html', {'form': form})
+
+
+@login_required  # usuarios autenticados puedan acceder
+def listar_vehiculos(request):
+    vehiculos = Vehiculo.objects.all()  # Obtiene todos los vehículos
+
+    # Clasifica los vehículos por precio
+    precios = []
+    for vehiculo in vehiculos:
+        if vehiculo.precio < 10000:
+            precios.append((vehiculo, 'Bajo'))
+        elif 10000 <= vehiculo.precio <= 30000:
+            precios.append((vehiculo, 'Medio'))
+        else:
+            precios.append((vehiculo, 'Alto'))
+
+    return render(request, 'vehiculo/listar_vehiculos.html', {'precios': precios})
